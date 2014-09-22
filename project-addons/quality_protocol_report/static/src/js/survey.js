6077e858cd551b8cdb753def24cc0de4cdca5c2e
jesusVMayor <jesus@pexego.es>
2014-09-22 17:49:55 +0200

    [UPD]modificado js.

--- project-addons/quality_protocol_report/static/src/js/survey.js
+++ project-addons/quality_protocol_report/static/src/js/survey.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function() {
     'use strict';
     $("#send_form").click(send_form_server);
     // Si el div #exist existe se rellenan las preguntas por ajax.
-    if($("#exist").length != 0) {
+    if ($("#exist").length != 0) {
         $("#all_data :input").attr("disabled", true);
         $("#send_form").hide();
         fill_data();
@@ -10,22 +10,24 @@ $(document).ready(function () {
 });
 
 
-function fill_data(){
-     $('#all_data').find('.survey').each(function(){
+function fill_data() {
+    $('#all_data').find('.survey').each(function() {
         var url_fill = $(this).attr("fill");
 
-        $(this).find("form").each(function(){
+        $(this).find("form").each(function() {
             var the_form = $(this);
-            $.ajax(url_fill, {dataType: "json"})
-                .done(function(json_data){
-                    _.each(json_data, function(value, key){
+            $.ajax(url_fill, {
+                    dataType: "json"
+                })
+                .done(function(json_data) {
+                    _.each(json_data, function(value, key) {
                         the_form.find(".form-control[name=" + key + "]").val(value);
-                        the_form.find("input[name^=" + key + "]").each(function(){
+                        the_form.find("input[name^=" + key + "]").each(function() {
                             $(this).val(value);
                         });
                     });
                 })
-                .fail(function(){
+                .fail(function() {
                     console.warn("[survey] Unable to load prefill data");
                 });
         });
@@ -36,44 +38,46 @@ function fill_data(){
   Se hace una llamada por encuesta y página.
   Luego se llama a una función del modulo para que relacione el numero de serie con las respuestas.
 */
-function send_form_server(){
+function send_form_server() {
     $('.js_errzone').html("").hide();
-    $('#all_data').find('.survey').each(function(){
+    $('#all_data').find('.survey').each(function() {
         var url_submit = $(this).attr("url_submit");
         var dat = $(this).find("form").serialize();
         // Si se cambia a disabled antes de serializar falla
         $(this).find(":input").attr("disabled", true);
-        $(this).find("form").each(function(){
+        $("#send_form").hide();
+        $(this).find("form").each(function() {
             $.ajax({
                 url: url_submit,
-                type: 'POST',                       // submission type
+                type: 'POST', // submission type
                 data: dat,
-                dataType: 'json',                   // answer expected type
-                beforeSubmit: function(){           // hide previous errmsg before resubmitting
+                dataType: 'json', // answer expected type
+                beforeSubmit: function() { // hide previous errmsg before resubmitting
                     $('.js_errzone').html("").hide();
                     console.log("entra aqui")
                 },
-                success: function(response, status, xhr, wfe){ // submission attempt
+                success: function(response, status, xhr, wfe) { // submission attempt
                     console.log("entra en success")
-                    if(_.has(response, 'errors')){  // some questions have errors
+                    if (_.has(response, 'errors')) { // some questions have errors
                         $("#all_data :input").attr("disabled", false);
-                        _.each(_.keys(response.errors), function(key){
+                        $("#send_form").show();
+                        _.each(_.keys(response.errors), function(key) {
                             $("#" + key + '>.js_errzone').append('<p>' + response.errors[key] + '</p>').show();
                         });
-                    }
-                    else if(_.has(response, 'redirect')){
+                    } else if (_.has(response, 'redirect')) {
                         //No se tienen en cuenta los mensajes de redireccion.
-                    }
-                    else {                                      // server sends bad data
+                    } else { // server sends bad data
                         console.error("Incorrect answer sent by server");
                         $("#all_data :input").attr("disabled", false);
+                        $("#send_form").show();
                     }
                 },
                 timeout: 5000,
-                error: function(jqXHR, textStatus, errorThrown){ // failure of AJAX request
+                error: function(jqXHR, textStatus, errorThrown) { // failure of AJAX request
                     console.log("entra en fallo ajax")
                     $('#AJAXErrorModal').modal('show');
                     $("#all_data :input").attr("disabled", false);
+                    $("#send_form").show();
                 }
             });
         });
@@ -81,4 +85,3 @@ function send_form_server(){
     });
 
 }
-
