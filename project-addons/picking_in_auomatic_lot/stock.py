548c95fd48528e36c0620a133a48d0cc306fa20f
jesusVMayor <jesus@pexego.es>
2014-12-29 19:21:21 +0100

    [ADD]picking_in_automatic_lot: Añadido lote de entrada automático.

---
+++ project-addons/picking_in_auomatic_lot/stock.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Pexego All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@pexego.es>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api
+
+
+class stock_transfer_details(models.TransientModel):
+    _inherit = 'stock.transfer_details'
+
+    @api.model
+    def default_get(self, fields):
+        res = super(stock_transfer_details, self).default_get(fields)
+        picking = self.env['stock.picking'].browse(self.env.context.get('active_id', False))
+        if picking.picking_type_code == 'incoming':
+            for item in res.get('item_ids', []):
+                if not item['product_id']:
+                    continue
+                prodlot_id = self.env['stock.production.lot'].with_context({'product_id': item['product_id']}).create({'product_id': item['product_id']})
+                prodlot_id.signal_workflow('in_revision')
+                item['lot_id'] = prodlot_id.id
+        return res
