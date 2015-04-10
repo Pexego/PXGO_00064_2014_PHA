e2be0a4bc1676da9d9f8256e05604f8d766fb17a
jesusVMayor <jesus@pexego.es>
2015-04-10 11:39:19 +0200

    [ADD]sequence_year_last_digit: Añadido modulo para crear secuencias con el último dígito del año.

---
+++ project-addons/sequence_year_last_digit/ir_sequence.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Pexego All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@pexego.es>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models
+import time
+
+
+class ir_sequence(models.Model):
+
+    _inherit = 'ir.sequence'
+
+    def _interpolation_dict(self):
+        res = super(ir_sequence, self)._interpolation_dict()
+        t = time.localtime()  # Actually, the server is always in UTC.
+        res['year_last'] = time.strftime('%y', t)[1]
+        return res
