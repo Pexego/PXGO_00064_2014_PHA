4977b598e1e9c58b40b5c38773d9e97c2660528c
Oscar Salvador <oscar.salvador@pharmadus.com>
2016-02-18 11:37:53 +0100

    New module to notify for changed fields in product.product model data

---
+++ project-addons/product_notify_changes/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2016 Pharmadus. All Rights Reserved
+#    $Óscar Salvador <oscar.salvador@pharmadus.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Product notify changes',
+    'version': '1.0',
+    'author': 'Pharmadus I+D+i',
+    'summary' : 'Create messages to notify changes in products data',
+    'description': 'Create messages to notify changes in products data',
+    'category': 'Social Network',
+    'website': 'www.pharmadus.com',
+    'depends' : [
+        'mail',
+        'product',
+    ],
+    'data' : [
+    ],
+    'installable': True
+}
