07f92550e1c41dbfbedc43c54f4c7b3e0e61e61f
jesusVMayor <jesus@comunitea.com>
2016-04-18 12:35:31 +0200

    [MOD]Eliminado stock_valued_pciking de repositorio local para usar el de external_modules.

--- project-addons/asperience_edi/__openerp__.py
+++ project-addons/asperience_edi/__openerp__.py
@@ -28,7 +28,7 @@
     "certificate": "",
     "license": "",
     "depends": ["board", "account_payment_sale", "sale",
-                "stock_valued_picking", 'commercial_and_financial_discount'],
+                "stock_picking_valued", 'commercial_and_financial_discount'],
     "category": "Generic Modules/EDI",
     "complexity": "easy",
     "description": """
