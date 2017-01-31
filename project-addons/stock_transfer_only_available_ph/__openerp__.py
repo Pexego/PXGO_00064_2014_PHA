67dd284298c1c1a1a4b931dbde3134c4ad7195dc
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-01-31 11:08:03 +0100

    Modified picking transfer wizard to show and check all internal locations stock

---
+++ project-addons/stock_transfer_only_available_ph/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea & Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock transfer only available products (PHARMADUS)',
+    'version': '1.1',
+    'category': 'stock',
+    'description': """Prevents transfer stock unavailable""",
+    'author': 'Comunitea & Pharmadus I.T.',
+    'website': '',
+    "depends": ['stock'],
+    "data": ['wizard/stock_transfer_details.xml'],
+    "installable": True
+}
