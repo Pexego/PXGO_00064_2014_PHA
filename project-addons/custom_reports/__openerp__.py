8129abe9599bf3ddc59a9325fe41b492cfded949
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-02-07 09:20:24 +0100

    Minor changes to rename view

--- project-addons/custom_reports/__openerp__.py
+++ project-addons/custom_reports/__openerp__.py
@@ -1,28 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Pharmadus All Rights Reserved
-#    $Óscar Salvador <oscar.salvador@pharmadus.com>$
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2017 Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Custom reports',
     'version': '1.0',
-    'author': 'Pharmadus I+D+i',
+    'author': 'Pharmadus I.T.',
     'summary' : 'Odoo reports customized',
     'description': 'Odoo reports customized',
     'category': 'Reports',
