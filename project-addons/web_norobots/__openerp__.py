88286b690481e8ef5f4e1ac06975d4c9d4ff0630
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-02-09 09:43:39 +0100

    Disable web robots indexing

---
+++ project-addons/web_norobots/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 B-Informed B.V. (<http://b-informed.nl>).
+#    Author: Roel Adriaans
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Alter robots.txt disallow all indexing',
+    'summary': 'Disable robots.txt for indexing',
+
+    'version': '1.0',
+    'author': 'B-Informed B.V.',
+    'complexity': 'normal',
+    'description': """
+This modules alters the /robots.txt view do disallow all indexing.
+
+This is usefull for test systems or websites you do not want indexed.
+    """,
+    'category': 'Website',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/disable_robots.xml',
+    ],
+    'auto_install': False,
+}
