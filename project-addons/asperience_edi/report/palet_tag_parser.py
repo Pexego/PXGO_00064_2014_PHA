fb6c800483029f07e3836ddf9bedcf9c001e22eb
javierjcf <javierjcf@gmail.com>
2017-02-24 16:00:59 +0100

    [ADD]aesperince_edi: Parseada info de informes de etiquetas

--- project-addons/asperience_edi/report/palet_tag_parser.py
+++ project-addons/asperience_edi/report/palet_tag_parser.py
@@ -19,15 +19,55 @@ class PaletTagParser(models.AbstractModel):
         if not data:
             raise except_orm(_('Error'),
                              _('You must print it from a wizard'))
-
+        # import ipdb; ipdb.set_trace()
         pick = pick_obj.browse(data['pick_id'])
+        package_dic = palet_dic = {}
+        num_palets = palet_number = 0
 
-        print "ENTRO EN PARSER"
+        for op in pick.pack_operation_ids:
+            # ETIQUETAS DE BULTO
+            if op.product_id not in package_dic:
+                package_dic[op.product_id] = []
+
+            num_packs = op.complete + (1 if op.package else 0)
+            for i in range(2 * num_packs):
+                dic = {
+                    'description': op.product_id.name,
+                    'serial_number': op.product_id.ean13,
+                    'lot': op.lot_id.name if op.lot_id else '-',
+                    'num_units': '12/36',
+                    'barcode': '184 33329 03229 9',
+                }
+                package_dic[op.product_id].append(dic)
+
+            # ETIQUETAS DE PALET
+            if not op.palet:
+                continue
+            if op.palet not in palet_dic:
+                palet_number += 1
+                num_palets += 1
+                palet_dic[op.palet] = {
+                    'place': 'ALMACÉN VALDEMORO 1 050',
+                    'place_dir': 'CTRA ANDALUCÍA K23, MARGEN IZQ;\
+                                  28340_VALDEMORO\n(Madrid)',
+                    'num_packs': 0,
+                    'palet_number': palet_number,
+                    'barcode': '',
+                }
+
+            num_packs = op.complete + (1 if op.package else 0)
+            palet_dic[op.palet]['num_packs'] += num_packs
+            if op.sscc:
+                palet_dic[op.palet]['barcode'] += op.sscc
+
         docargs = {
             'doc_ids': [],
             'doc_model': 'stock.picking',
             'docs': pick,
-            'mi_string': "MI STRING",
-            'mi_string2': data['string2']
+            'package_dic': package_dic,
+            'palet_dic': palet_dic,
+            'num_palets': num_palets
         }
+        # import ipdb; ipdb.set_trace()
+        print package_dic
         return report_obj.render(report_name, docargs)
