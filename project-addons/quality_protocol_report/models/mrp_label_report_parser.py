575d256ad229faa670f45f46e97dfe04268a145d
jesusVMayor <jesus@comunitea.com>
2017-04-05 11:42:43 +0200

    [FIX]quality_protocol_report: Correcciones en etiquetas.

--- project-addons/quality_protocol_report/models/mrp_label_report_parser.py
+++ project-addons/quality_protocol_report/models/mrp_label_report_parser.py
@@ -13,10 +13,27 @@ class MrpLabelReport(models.AbstractModel):
     def render_html(self, data=None):
         report_obj = self.env['report']
         report = report_obj._get_report_from_name('quality_protocol_report.report_mrp_label')
+        doc = self.env[report.model].browse(data['ids'])
+        name_css_size = 52
+        if len(doc.product_id.name) > 51:
+            name_css_size = 22
+        elif len(doc.product_id.name) > 39:
+            name_css_size = 32
+        elif len(doc.product_id.name) > 31:
+            name_css_size = 42
+
+        lot_css_size = 140
+        if len(doc.final_lot_id.name) > 14:
+            lot_css_size = 90
+        elif len(doc.final_lot_id.name) > 11:
+            lot_css_size = 110
+
         docargs = {
             'doc_ids': data['ids'],
             'doc_model': report.model,
-            'docs': self.env[report.model].browse(data['ids']),
+            'docs': doc,
             'gtin': data.get('gtin', False),
+            'name_css_size': str(name_css_size),
+            'lot_css_size': str(lot_css_size)
         }
         return report_obj.render('quality_protocol_report.report_mrp_label', docargs)
