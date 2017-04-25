3aa6f6848b55e53ba8047ec3892592cd6986b008
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-04-25 14:20:25 +0200

    Add duration type field to configure calculations of product duration date.

---
+++ project-addons/product_preferential_consumption/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product preferential consumption extra data',
+    'version': '1.0',
+    'category': 'mrp',
+    'summary' : 'Computes preferential consumption date dependign on the option selected in duration type',
+    'description': "Computes preferential consumption date dependign on the option selected in duration type",
+    'icon': '',
+    'author': 'Pharmadus I.T.',
+    'website': 'www.pharmadus.com',
+    'depends': [
+        'product',
+        'stock'
+    ],
+    'data': [
+        'views/product_view.xml',
+    ],
+    'installable': True
+}
