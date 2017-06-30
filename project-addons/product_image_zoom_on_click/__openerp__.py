43255ccd10cb10254a953570bd42dcd9eff1f9d5
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-06-30 13:17:20 +0200

    New module to zoom product image in form

---
+++ project-addons/product_image_zoom_on_click/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea & Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Product image zoom',
+    'category': 'Backend',
+    'version': '1.0',
+    'author': 'Pharmadus I.T.',
+    'website': 'https://www.pharmadus.com',
+    "description": """Using this module you can zoom image of product on mouse click.""",
+    "summary": "This module can be useful for adding image zoom functionality on mouse click.",
+    'depends': ['product'],
+    'data': [
+        'views/template.xml',
+    ],
+    'images': [
+        'static/description/icon.png',
+    ],
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
