c9d80f27eabf8baaf643e94a57d6c08bb205f13d
Oscar Salvador <oscar.salvador@pharmadus.com>
2017-09-12 10:12:13 +0200

    New view that shows pricelists products prices, to facilitate pricelist export.

---
+++ project-addons/custom_views/models/product_pricelist.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# © 2017 Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import models, fields, api, _
+
+
+class ProductPricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    pricelist_id = fields.Many2one(related='price_version_id.pricelist_id')
