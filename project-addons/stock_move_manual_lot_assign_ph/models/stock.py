77e93fae8d47c3d8387a4fc712cc57c71e142123
Oscar Salvador <oscar.salvador@pharmadus.com>
2018-09-05 08:13:29 +0200

    En las movimientos de un albarán, mostramos una columna con el stock disponible para los lotes reservados según los cálculos del asistente de asignación de lotes manual.

--- project-addons/stock_move_manual_lot_assign_ph/models/stock.py
+++ project-addons/stock_move_manual_lot_assign_ph/models/stock.py
@@ -10,6 +10,8 @@ class StockMove(models.Model):
 
     show_manual_lot_assign_button = fields.Boolean(
         compute='_show_manual_lot_assign_button')
+    reserved_lots_available_qty = fields.Float(string='Available',
+        compute='_reserved_lots_available_qty')
 
     @api.one
     def _show_manual_lot_assign_button(self):
@@ -23,6 +25,20 @@ class StockMove(models.Model):
                 )
             ) and \
             self.picking_id.state in ('assigned', 'partially_available')
+
+    @api.one
+    def _reserved_lots_available_qty(self):
+        if self.lot_ids:
+            wizard_id = self.env['stock.move.assign.manual.lot'].\
+                with_context({'active_id': self.id, 'compute_only': True}).\
+                create({})
+            available_qty = 0
+            for lot in self.lot_ids:
+                wizard_line_ids = wizard_id.line_ids.\
+                    filtered(lambda l: l.lot_id == lot)
+                if wizard_line_ids:
+                    available_qty += sum(wizard_line_ids.mapped('available_qty'))
+            self.reserved_lots_available_qty = available_qty
 
     @api.multi
     def action_manual_lot_assign_wizard(self):
