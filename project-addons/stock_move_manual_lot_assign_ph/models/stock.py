b4fe814d5ef55ed595f5ada2b5c137daedc49b17
Oscar Salvador <oscar.salvador@pharmadus.com>
2018-10-16 21:10:19 +0200

    Solución al "bug" aleatorio del cálculo del campo disponible en movimientos de albaranes internos

--- project-addons/stock_move_manual_lot_assign_ph/models/stock.py
+++ project-addons/stock_move_manual_lot_assign_ph/models/stock.py
@@ -31,27 +31,31 @@ class StockMove(models.Model):
     def _reserved_lots_available_qty(self):
         if self.picking_id and \
            self.picking_id.picking_type_code == 'internal' and \
-           self.is_hoard_move and \
            self.state in ('assigned', 'confirmed') and \
            self.lot_ids:
-            wizard_id = self.env['stock.move.assign.manual.lot'].\
-                with_context({'active_id': self.id, 'compute_only': True}).\
-                create({})
-            available_qty = 0
-            for lot in self.lot_ids:
-                wizard_line_ids = wizard_id.line_ids.\
-                    filtered(lambda l: l.lot_id == lot)
-                if wizard_line_ids:
-                    available_qty += sum(wizard_line_ids.mapped('available_qty'))
-            self.reserved_lots_available_qty = available_qty
+            quant_ids = self.env['stock.quant'].search([
+                ('lot_id', 'in', self.lot_ids.ids),
+                ('location_id', 'child_of', self.location_id.id),
+                ('product_id', '=', self.product_id.id),
+                ('qty', '>', 0),
+                '|',
+                ('reservation_id', '=', False),
+                ('reservation_id', '=', self.id)
+            ])
+            self.reserved_lots_available_qty = sum(quant_ids.mapped('qty'))
 
             # Check if any old lot is unused
-            wizard_line_ids = wizard_id.line_ids.sorted(key=lambda r: r.lot_id)
-            lines_count = len(wizard_line_ids)
-            flag = False
-            for idx, line_id in enumerate(wizard_line_ids, start=1):
-                flag = flag or ((idx != lines_count) and (line_id.use_qty == 0.0))
-            self.old_lots_unused = flag
+            quant_ids = self.env['stock.quant'].search([
+                ('lot_id', 'not in', self.lot_ids.ids),
+                ('location_id', 'child_of', self.location_id.id),
+                ('product_id', '=', self.product_id.id),
+                ('qty', '>', 0),
+                '|',
+                ('reservation_id', '=', False),
+                ('reservation_id', '=', self.id)
+            ])
+            self.old_lots_unused = (quant_ids and \
+                min(quant_ids.mapped('lot_id.id')) < min(self.lot_ids.ids))
 
     @api.multi
     def action_manual_lot_assign_wizard(self):
