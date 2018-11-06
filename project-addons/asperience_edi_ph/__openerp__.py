affc35995f30d7e03a069ee934a52cb113523019
Oscar Salvador <oscar.salvador@pharmadus.com>
2018-11-06 14:39:29 +0100

    Se extrae la vista con los carteles de EDI y DESADV a un nuevo módulo para evitar referencias circulares entre asperience_edi y custom_views

---
+++ project-addons/asperience_edi_ph/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2018 Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Asperience EDI PH',
+    'version': '1.0',
+    'author': 'Pharmadus I.T.',
+    'summary' : 'Inherits Asperience EDI module to add Pharmadus customizations',
+    'description': 'Inherits Asperience EDI module to add Pharmadus customizations',
+    'category': 'General',
+    'website': 'www.pharmadus.com',
+    'depends' : [
+        'asperience_edi',
+    ],
+    'data' : [
+        'views/stock_view.xml',
+    ],
+    'installable': True
+}
