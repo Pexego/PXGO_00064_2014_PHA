672458289789aa2a8280c7f8e3f923c8dc808709
Oscar Salvador <oscar.salvador@pharmadus.com>
2019-02-07 10:31:31 +0100

    Nuevo módulo para desactivar botones borrar y exportar del menú desplegable en las vistas lista.

---
+++ project-addons/web_disable_export_delete_button/static/src/js/disable_export_delete_button.js
@@ -0,0 +1,27 @@
+openerp.web_disable_export_delete_button = function(instance) {
+    var _t = instance.web._t;
+
+    instance.web.Sidebar.include({
+        add_items: function(section_code, items) {
+            var hasExportButton = $('span#web_disable_export_delete_button').attr('user_in_export_group') == 1;
+            var hasDeleteButton = $('span#web_disable_export_delete_button').attr('user_in_delete_group') == 1;
+
+            if ((this.session.uid == 1) || (hasExportButton && hasDeleteButton)) {
+                this._super.apply(this, arguments);
+            } else {
+                var new_items = items;
+                if (section_code == 'other') {
+                    new_items = [];
+                    for (var i = 0; i < items.length; i++) {
+                        if (!hasExportButton && items[i]['label'] == _t('Export')) { continue; }
+                        if (!hasDeleteButton && items[i]['label'] == _t('Delete')) { continue; }
+                        new_items.push(items[i]);
+                    };
+                };
+                if (new_items.length > 0) {
+                    this._super.call(this, section_code, new_items);
+                };
+            }
+        },
+    });
+};
