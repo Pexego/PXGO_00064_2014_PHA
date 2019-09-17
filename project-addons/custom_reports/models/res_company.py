69c412147c7b2283f5836c2e2faf6bd474855bfb
Oscar Salvador <oscar.salvador@pharmadus.com>
2019-09-17 13:59:24 +0200

    Mensajes de pie de página de pedidos, facturas y albaranes, configurables por compañía e idioma del partner al que van dirigidos.

--- project-addons/custom_reports/models/res_company.py
+++ project-addons/custom_reports/models/res_company.py
@@ -8,11 +8,13 @@ from openerp import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    report_header_message = fields.Text()
-    report_sales_footer_message = fields.Text()
-    report_purchases_footer_message = fields.Text()
-    report_sales_email = fields.Char()
-    report_purchases_email = fields.Char()
+    report_header_message = fields.Text(translate=True)
+    report_sales_footer_message = fields.Text(translate=True)
+    report_purchases_footer_message = fields.Text(translate=True)
+    report_gdpr_footer_message = fields.Text(translate=True)
+    report_waste_disposal_footer_message = fields.Text(translate=True)
+    report_sales_email = fields.Char(translate=True)
+    report_purchases_email = fields.Char(translate=True)
     report_product_category_message = fields.One2many(
         comodel_name='report.product.category.message',
         inverse_name='company_id')
