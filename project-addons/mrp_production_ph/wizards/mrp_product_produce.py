2a9f403531b34288c2bbdb1c823b6e7496d717ac
Oscar Salvador <oscar.salvador@pharmadus.com>
2019-11-08 14:20:45 +0100

    Al finalizar la producción, se traslada el valor del campo tara del wizard a la producción

--- project-addons/mrp_production_ph/wizards/mrp_product_produce.py
+++ project-addons/mrp_production_ph/wizards/mrp_product_produce.py
@@ -2,10 +2,20 @@
 # © 2019 Pharmadus I.T.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class MrpProductProduce(models.TransientModel):
     _inherit = 'mrp.product.produce'
 
     tare = fields.Float()
+
+    @api.multi
+    def do_produce(self):
+        res = super(MrpProductProduce, self).do_produce()
+        production_id = self.env['mrp.production'].browse(
+            self.env.context.get('active_id', False)
+        )
+        if production_id and self.tare:
+            production_id.tare = self.tare
+        return res
