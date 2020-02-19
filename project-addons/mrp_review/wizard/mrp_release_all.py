901d9d84c6f8d2e229b548607c9ca159606f9bd9
JesusVMayor <jesus@comunitea.com>
2020-02-19 13:58:30 +0100

    [FIX]mrp_review: Corregido nuevo flujo de producciones.

--- project-addons/mrp_review/wizard/mrp_release_all.py
+++ project-addons/mrp_review/wizard/mrp_release_all.py
@@ -49,7 +49,7 @@ class MrpReleaseAll(models.TransientModel):
         produce_wiz.with_context(
             ignore_child=True, not_cancel=True).do_produce()
         mrp.move_created_ids.action_cancel()
-        if mrp.postposed_release:
+        if not mrp.postposed_release:
             mrp.signal_workflow('button_release_all')
         else:
             mrp.signal_workflow('button_produce_done')
