1f65e4905842cefbef04d1e3f0c1994e620f42c2
Oscar Salvador <oscar.salvador@pharmadus.com>
2020-07-30 11:19:28 +0200

    Mostramos el campo disponible en la ventana de transferencia con código de barras

--- project-addons/mrp_production_ph/wizards/stock_transfer_details.py
+++ project-addons/mrp_production_ph/wizards/stock_transfer_details.py
@@ -58,3 +58,21 @@ class StockTransferDetailsItems(models.TransientModel):
     _inherit = 'stock.transfer_details_items'
 
     lot_barcode = fields.Char()
+    available_qty = fields.Float(compute='_get_available_qty', readonly=True)
+
+    @api.one
+    def _get_available_qty(self):
+        move_ids = []
+        if self.packop_id:
+            for m in self.packop_id.picking_id.move_lines:
+                if m.product_id == self.product_id:
+                    move_ids += [m.id]
+        available_quants = self.env['stock.quant'].search([
+            ('lot_id', '=', self.lot_id.id),
+            ('location_id', 'child_of', self.sourceloc_id.id),
+            ('product_id', '=', self.product_id.id),
+            ('qty', '>', 0),
+            '|',
+            ('reservation_id', '=', False),
+            ('reservation_id', 'in', move_ids)])
+        self.available_qty = sum(available_quants.mapped('qty'))
