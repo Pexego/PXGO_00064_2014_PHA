31b1f73846f731b8d05de8991b9c9c63c5244949
Oscar Salvador <oscar.salvador@pharmadus.com>
2022-06-18 09:29:50 +0200

    Módulo que guarda modificaciones sobre las LdM en su área de mensajes

---
+++ project-addons/mrp_bom_notify_changes/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2022 Pharmadus I.T.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'MRP BoM notify changes',
+    'version': '1.0',
+    'author': 'Pharmadus I+D+i',
+    'summary' : 'Create messages to notify changes in MRP Bills of Materials data',
+    'description': 'Create messages to notify changes in MRP Bills of Materials data',
+    'category': 'Others',
+    'website': 'www.pharmadus.com',
+    'depends' : [
+        'mrp',
+    ],
+    'data' : [
+    ],
+    'installable': True
+}
