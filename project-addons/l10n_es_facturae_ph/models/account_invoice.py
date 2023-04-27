6c6d260a96214f4e9ba75fca752b91109e1cf162
Oscar Salvador <oscar.salvador@pharmadus.com>
2023-04-27 10:59:59 +0200

    Añadido período de facturación en Factura-e y papelera para reiniciar entrada de datos

--- project-addons/l10n_es_facturae_ph/models/account_invoice.py
+++ project-addons/l10n_es_facturae_ph/models/account_invoice.py
@@ -2,14 +2,27 @@
 # © 2022 Pharmadus Botanicals
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, exceptions
+from openerp import models, api
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
-    def facturae_lines(self):
+    def facturae_detail(self):
+        wizard_data = {
+            'name': self.number,
+            'invoice_id': self.id,
+            'partner': self.partner_id.name,
+        }
+
+        invoice_id = self.env['facturae.invoice'].search([
+            ('invoice_id', '=', self.id)
+        ])
+        if invoice_id:
+            wizard_data['start_period'] = invoice_id.start_period
+            wizard_data['end_period'] = invoice_id.end_period
+
         line_ids = []
         for line_id in self.invoice_line:
             line_ids += [(0, 0, {
@@ -18,12 +31,9 @@ class AccountInvoice(models.Model):
                 'description': line_id.name,
                 'quantity': line_id.quantity
             })]
+        wizard_data['line_ids'] = line_ids
 
-        wizard = self.env['facturae.invoice.lines.wizard'].create({
-            'name': self.number,
-            'partner': self.partner_id.name,
-            'line_ids': line_ids
-        })
+        wizard = self.env['facturae.invoice.lines.wizard'].create(wizard_data)
 
         return {
             'type': 'ir.actions.act_window',
@@ -37,11 +47,13 @@ class AccountInvoice(models.Model):
         }
 
     @api.multi
-    def facturae_clear_lines(self):
+    def facturae_clear(self):
+        self.env['facturae.invoice'].search([(['invoice_id', '=', self.id])]).\
+            unlink()
         invoice_line_ids = [line_id.id for line_id in self.invoice_line]
         self.env['facturae.invoice.lines']. \
             search([('invoice_line_id', 'in', invoice_line_ids)]).unlink()
         return self.env['custom.views.warning'].show_message(
             'Factura-e',
-            'Se ha limpiado el detalle de líneas de la factura-e'
+            'Se han limpiado todos los datos de la factura-e'
         )
--- project-addons/l10n_es_facturae_ph/models/facturae_invoice.py
+++ project-addons/l10n_es_facturae_ph/models/facturae_invoice.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
-# © 2022 Pharmadus Botanicals
+# © 2023 Pharmadus Botanicals
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, fields
+from openerp import models, fields
+
+
+class FacturaeInvoice(models.Model):
+    _name = 'facturae.invoice'
+
+    invoice_id = fields.Integer(index=True)
+    start_period = fields.Date('desde')
+    end_period = fields.Date('a')
 
 
 class FacturaeInvoiceLines(models.Model):
     _name = 'facturae.invoice.lines'
 
+    invoice_id = fields.Integer(index=True)
     invoice_line_id = fields.Integer(index=True)
     contract_reference = fields.Char()
     transaction_reference = fields.Char()
